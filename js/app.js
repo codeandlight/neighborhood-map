02d49b1cdffa0ecf5092105215219ec195aef2e7
Jun Clemente <junclemente@outlook.com>
2017-03-03 17:34:53 -0800

    add comments

--- js/app.js
+++ js/app.js
@@ -4,22 +4,122 @@ Google Maps Javascript
 
 // List of Mammoth Lakes Locations
 var placesInMammoth = [
-    { name: 'Cinnamon Bear Inn', phone: '7609342873', listView: true, locationType: 'Lodging', location: { lat: 37.646934, lng: -118.971888 }},
-    { name: 'Quality Inn', phone: '7609345114', listView: true, locationType: 'Lodging', location: { lat: 37.648242, lng: -118.975139 }},
-    { name: 'Innsbruck Lodge', phone: '7609343035', listView: true, locationType: 'Lodging', location: { lat: 37.651181, lng: -118.982188 }},
-    { name: 'Rodeway Inn Wildwood Inn', phone: '7609346855', listView: true, locationType: 'Lodging', location: { lat: 37.649133, lng: -118.976446 }},
-    { name: 'M Inn Mammoth', phone: '7609342710', listView: true, locationType: 'Lodging', location: { lat: 37.647426, lng: -118.976800 }},
-    { name: 'Shilo Inn Suites', phone: '7609650544', listView: true, locationType: 'Lodging', location: { lat: 37.646093, lng: -118.964614 }},
-    { name: 'Travelodge', phone: '8007606483', listView: true, locationType: 'Lodging', location: { lat: 37.649304, lng: -118.974173 }},
-    { name: 'Alpenhof Lodge', phone: '7609346330', listView: true, locationType: 'Lodging', location: { lat: 37.650034, lng: -118.983540 }},
-    { name: "Giovanni's Pizzeria", phone: '7609347563', listView: true, locationType: 'Restaurant', location: { lat: 37.639957, lng: -118.964398 }},
-    { name: "Roberto's Mexican Cafe", phone: '7609343667', listView: true, locationType: 'Restaurant', location: { lat: 37.642279, lng: -118.965988 }},
-    { name: 'Starbucks', phone: '7609344536', listView: true, locationType: 'Restaurant', location: { lat: 37.642279, lng: -118.965988 }},
-    { name: 'Mammoth Brewing Company', phone: '7609347141', listView: true, locationType: 'Restaurant', location: { lat: 37.648632, lng: -118.983435 }},
-    { name: 'Minaret Village Shopping Center', phone: '7609346005', listView: true, locationType: 'Shopping', location: { lat: 37.639138, lng: -118.964294 }},
-    { name: 'Vons / Starbucks', phone: '7609344536', listView: true, locationType: 'Shopping', location: { lat: 37.638545, lng: -118.965243 }},
-    { name: 'Mammoth Fun Shop', phone: '7609241111', listView: true, locationType: 'Shopping', location: { lat: 37.646967, lng: -118.968285 }},
-    { name: "McCoy's Mammoth Memories", phone: '7609247070', listView: true, locationType: 'Shopping', location: { lat: 37.650671, lng: -118.985323 }},
+    {
+        name: 'Cinnamon Bear Inn',
+        phone: '7609342873',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.646934, lng: -118.971888 }
+    },
+    {
+        name: 'Quality Inn',
+        phone: '7609345114',
+
+        listView: true,
+
+        locationType: 'Lodging',
+
+        location: { lat: 37.648242, lng: -118.975139 }
+
+    },
+    {
+        name: 'Innsbruck Lodge',
+        phone: '7609343035',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.651181, lng: -118.982188 }
+    },
+    {
+        name: 'Rodeway Inn Wildwood Inn',
+        phone: '7609346855',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.649133, lng: -118.976446 }
+    },
+    {
+        name: 'M Inn Mammoth',
+        phone: '7609342710',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.647426, lng: -118.976800 }
+    },
+    {
+        name: 'Shilo Inn Suites',
+        phone: '7609650544',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.646093, lng: -118.964614 }
+    },
+    {
+        name: 'Travelodge',
+        phone: '8007606483',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.649304, lng: -118.974173 }
+    },
+    {
+        name: 'Alpenhof Lodge',
+        phone: '7609346330',
+        listView: true,
+        locationType: 'Lodging',
+        location: { lat: 37.650034, lng: -118.983540 }
+    },
+    {
+        name: "Giovanni's Pizzeria",
+        phone: '7609347563',
+        listView: true,
+        locationType: 'Restaurant',
+        location: { lat: 37.639957, lng: -118.964398 }
+    },
+    {
+        name: "Roberto's Mexican Cafe",
+        phone: '7609343667',
+        listView: true,
+        locationType: 'Restaurant',
+        location: { lat: 37.642279, lng: -118.965988 }
+    },
+    {
+        name: 'Starbucks',
+        phone: '7609344536',
+        listView: true,
+        locationType: 'Restaurant',
+        location: { lat: 37.642279, lng: -118.965988 }
+    },
+    {
+        name: 'Mammoth Brewing Company',
+        phone: '7609347141',
+        listView: true,
+        locationType: 'Restaurant',
+        location: { lat: 37.648632, lng: -118.983435 }
+    },
+    {
+        name: 'Minaret Village Shopping Center',
+        phone: '7609346005',
+        listView: true,
+        locationType: 'Shopping',
+        location: { lat: 37.639138, lng: -118.964294 }
+    },
+    {
+        name: 'Vons / Starbucks',
+        phone: '7609344536',
+        listView: true,
+        locationType: 'Shopping',
+        location: { lat: 37.638545, lng: -118.965243 }
+    },
+    {
+        name: 'Mammoth Fun Shop',
+        phone: '7609241111',
+        listView: true,
+        locationType: 'Shopping',
+        location: { lat: 37.646967, lng: -118.968285 }
+    },
+    {
+        name: "McCoy's Mammoth Memories",
+        phone: '7609247070',
+        listView: true,
+        locationType: 'Shopping',
+        location: { lat: 37.650671, lng: -118.985323 }
+    },
 ];
 
 // Variables placed in the global scope to ensure it only has one value at all times
@@ -28,11 +128,24 @@ var map, bounds, infowindow, infoWindowOptions, mapCenter;
 // Initialize markers array
 var markers = [];
 
-/*
-** This is the model for the markers that are placed on the Google Map and in the dropdown menu.
-*/
 var markerModel = function(item) {
-
+    /*
+    ** This is the model for the markers that are placed on the Google Map and in the dropdown menu.
+    ** Knockout observables:
+    **      name : name of location
+    **      phone : phone number of location, used for Yelp API business search
+    **      locationType : type of location , used for filtering marker and list view
+    **      listView : boolean variable used for filtering list view
+    **      location : Google LatLng Object
+    **      marker : Google Marker Object
+    **
+    ** previewOn(), previewOut(), selectMarker() : functions activated by Knockout events in the list view
+    **
+    ** makeMarkerIcon : creates a customized marker color activated by marker listener events
+    ** populateInfoWindow : activated by Knockout event in list view or marker listener event on the map,
+    **                      performs AJAX call to Yelp Business Search API using the phone number, parses the recieved
+    **                      data and creates the Google InfoWindow Object.
+    */
     var self = this;
 
     // Variables that define the custom marker look and calls makeMarkerIcon() to create them
@@ -43,9 +156,7 @@ var markerModel = function(item) {
     self.phone = ko.observable(item.phone);
     self.locationType = ko.observable(item.locationType);
     self.listView = ko.observable(item.listView);
-    // self.yelpId = ko.observable(item.yelpId);
     self.location = ko.observable(item.location);
-
     self.marker = ko.observable(new google.maps.Marker({
         position: this.location(),
         title: this.name(),
@@ -57,38 +168,37 @@ var markerModel = function(item) {
     // Add marker location to bounds to resize map and fit all markers
     bounds.extend(self.location());
 
-    // Add event listeners
+    // Add event listeners for markers on the map
     self.marker().addListener('mouseover', function() {
         this.setIcon(highlightedMarker);
     });
     self.marker().addListener('mouseout', function() {
         this.setIcon(defaultMarker);
     });
-
-    //
     self.marker().addListener('click', function() {
         populateInfoWindow();
     });
 
-
+    // Knockout events
     self.previewOn = function() {
         self.marker().setIcon(highlightedMarker);
         self.marker().setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker().setAnimation(-1);
+        }, 1400);
     };
-
     self.previewOut = function() {
         self.marker().setIcon(defaultMarker);
         self.marker().setAnimation(-1);
     };
-
     self.selectMarker = function() {
-        map.panTo(self.location());
         map.setZoom(16);
         self.marker().setIcon(highlightedMarker);
         self.marker().setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker().setAnimation(-1);
+        }, 1400);
         populateInfoWindow();
-        that.hideOtherMarkers(self);
-        console.log('clicked...');
     };
 
     // Changes the look of the marker icon
@@ -99,6 +209,7 @@ var markerModel = function(item) {
         return markerImage;
     }
 
+    // Populates infowindow after successful AJAX call to Yelp API
     function populateInfoWindow() {
         if (infowindow.marker === self.marker()) {
             console.log('marker already selected');
@@ -107,8 +218,7 @@ var markerModel = function(item) {
             infowindow.setContent('retrieving information...');
             map.panTo(self.location());
             var message = {
-                // 'action': 'https://api.yelp.com/v2/business/' + self.yelpId(),
-                'action': 'https://api.yelp.com/v2/phone_search?phone=+1' + self.phone(),
+                'action': 'https://api.yelp.com/v2/phone_search?phone=' + self.phone() + '&cc=US',
                 'method': 'GET',
                 'parameters': parameters
             };
@@ -125,9 +235,9 @@ var markerModel = function(item) {
             })
                 .fail( function( jqXHR, textStatus, errorThrown) {
                 console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+                infowindow.setContent('There was an error contacting Yelp...')
             })
                 .done( function(data, textStatus, jqXHR) {
-                    console.log(data.businesses[0]);
                     var businessInfo = JSON.parse(JSON.stringify(data.businesses[0]));
                     console.log('status[' + textStatus + ']');
                     var contentString = '<div class="yelpInfoWindow">' +
@@ -143,30 +253,36 @@ var markerModel = function(item) {
                 infowindow.setContent(contentString);
             });
             infowindow.open( map, self.marker() );
-            mapCenter = self.marker().position;
         }
 
         infowindow.addListener('closeclick', function() {
-            map.panTo(mapCenter);
             map.fitBounds(bounds);
             infowindow.marker = null;
         });
     }
 };
 
-
 var appViewModel = function() {
-    that = this;
+
+    /*
+    ** This is the view model for the Knockout bindings.
+    ** Knockout variables:
+    **      mapMarkers : Array of modelMarkers
+    **      locationType : Array of location types that used to filter mapMarkers
+    **      selectedLocationType : Observable to hold the value of the locationType for filtering
+    **      filterMarkers : Computed observable that filters the list view and sets the visibility of
+    **                      each marker
+    */
+    var that = this;
     that.mapMarkers = ko.observableArray([]);
-    that.searchInfo = ko.observable();
-    that.searchCenter = ko.observable();
-    that.searchResults = ko.observableArray([]);
-    that.viewInfo = ko.observable();
 
     that.locationType = ko.observableArray(['All', 'Lodging', 'Restaurant', 'Shopping']);
     that.selectedLocationType = ko.observable('All');
 
     that.filterMarkers = ko.computed(function() {
+        infowindow.close();
+        infowindow.marker = null;
+        map.fitBounds(bounds);
         for (var i = 0; i < that.mapMarkers().length; i++) {
             if (that.selectedLocationType() == 'All') {
                 that.mapMarkers()[i].marker().setMap(map);
@@ -196,10 +312,13 @@ var appViewModel = function() {
 
     // Resize map to fit all markers
     map.fitBounds(bounds);
-    mapCenter = map.getCenter();
 };
 
 function initMap() {
+
+    /*
+    ** This function initializes the Google Map API and KnockoutJS binding
+    */
 
     // Custom map styling created using https://mapstyle.withgoogle.com/ | minified
     var styles = [{"elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"color":"#eef0f0"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#cccccc"},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"geometry","stylers":[{"color":"#b9fa99"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#cbfac7"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#66ccff"}]},{"featureType":"road.arterial","stylers":[{"color":"#66ccff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#66ccff"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#000000"},{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#cdfdff"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}];
@@ -209,6 +328,10 @@ function initMap() {
         center: placesInMammoth[0].location,
         zoom: 20,
         mapTypeControl: false,
+        disableDoubleClickZoom: true,
+        fullscreentControl: false,
+        streetViewControl: false,
+        zoomControl: false,
         styles: styles
     });
 
